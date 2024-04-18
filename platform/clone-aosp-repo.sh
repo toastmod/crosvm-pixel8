6f11133c32787f2f8b81840a495a057f474dfae0
Andrew Numrich <toastymod@gmail.com>
2024-04-17 23:37:01 -0600

    Create clone-aosp-repo.sh

---
+++ platform/clone-aosp-repo.sh
@@ -0,0 +1,2 @@
+repo init -b android-14.0.0_r33 -u https://android.googlesource.com/platform/manifest
+repo sync platform/build platform/build/soong platform/build/bazel platform/build/blueprint platform/prebuilts/bazel/common platform/prebuilts/go/linux-x86 platform/prebuilts/build-tools platform/tools/asuite platform/system/core platform/packages/modules/adb platform/external/golang-protobuf platform/external/starlark-go platform/external/crosvm
